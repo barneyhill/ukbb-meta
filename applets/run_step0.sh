a80898a3bff06a54f292b1be82c497cc8b1a0cb1
Barney Hill <barneyhill1@gmail.com>
2022-10-06 11:57:01 +0100

    added step0 applet

---
+++ applets/run_step0.sh
@@ -0,0 +1,3 @@
+dx run applet-GGzBzG0Jg8JQBbV3PKKYzJ3q -i plink_files=ukbb-meta/data/preprocessing/*  \
+                                       --instance-type "mem3_ssd1_v2_x2" --priority high --destination ukbb-meta/data/step0/ --delay-workspace-destruction --ssh
+
